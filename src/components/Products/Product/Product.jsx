b93c6f58b35e928d13c8f00b42ab3a2e89e4e4fc
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-06 15:25:45 +0200

    Navbar

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,53 +11,36 @@ import {
 import { AddShoppingCart } from "@material-ui/icons";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running Shoes.",
-    price: "$5",
-    image:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/reebok-run-panthea-8990-1581455951.jpg?crop=0.895xw:0.895xh;0.0510xw,0.105xh&resize=980:*",
-  },
-  {
-    id: 2,
-    name: "MacBook",
-    description: "Apple MacBook.",
-    price: "$10",
-    image:
-      "https://static.bhphoto.com/images/multiple_images/images500x500/1584552724_IMG_1332174.jpg",
-  },
-];
-
-const Product = (props) => {
+const Product = ({ product }) => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <p>{props.product.name}</p>
-      <CardMedia
-        className={classes.media}
-        image={props.product.image}
-        title={props.product.name}
-      />
-      <CardContent>
-        <div className={classes.cardContent}>
-          <Typography variant="h5" gutterBottom>
-            {props.product.name}
+    <>
+      <Card className={classes.root}>
+        <p>{product.name}</p>
+        <CardMedia
+          className={classes.media}
+          image={product.image}
+          title={product.name}
+        />
+        <CardContent>
+          <div className={classes.cardContent}>
+            <Typography variant="h5" gutterBottom>
+              {product.name}
+            </Typography>
+            <Typography variant="h5">{product.price}</Typography>
+          </div>
+          <Typography variant="body2" color="textSecondary">
+            {product.description}
           </Typography>
-          <Typography variant="h5">{props.product.price}</Typography>
-        </div>
-        <Typography variant="body2" color="textSecondary">
-          {props.product.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing className={classes.CardActions}>
-        <IconButton aria-label="Add to Cart">
-          <AddShoppingCart />
-        </IconButton>
-      </CardActions>
-    </Card>
+        </CardContent>
+        <CardActions disableSpacing className={classes.CardActions}>
+          <IconButton aria-label="Add to Cart">
+            <AddShoppingCart />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
   {
@@ -21,9 +22,12 @@ const products = [
   },
 ];
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent="center" spacing={4}>
+        <p>Hello Testing</p>
         {products.map((product) => {
           <Grid item key={product.id} xs={10} sm={6} md={4} lg={3}>
             <Product product={product} />;
